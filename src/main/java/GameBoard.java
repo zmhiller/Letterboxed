553f9befcf3f073144d4cfb725fb8d2654c6f300
Zach Hiller <zmhiller@gmail.com>
2023-10-07 21:49:27 -0400

    Working on id method for illegal pairs

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,28 +1,48 @@
-import java.lang.reflect.Array;
-
 public class GameBoard {
 
     GameBoard(char[] letters) {
         char[][] sides = defineSides(letters);
     }
 
+    // i = side number (Top-0, Right-1, Bottom-2,Left-3
+    //j = letter position (clockwise)
     public static char[][] defineSides(char[] letters) {
         char[][] sides = new char[4][5];
         int l = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.printf("\ni = %d j = %d l = %d",i, j, l);
-                sides[j][i] = letters[l];
-                System.out.printf("[ %s ],[ %s ]: "+ sides[j][i],j,i);
+                sides[i][j] = letters[l];
                 l++;
             }
         }
         return sides;
     }
 
+    private static void printSides(char[][] sides) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("\n[ %s ],[ %s ]: "+ sides[i][j],i,j);
+            }
+        }
+    }
+
+    private static String[] defineIllegalPairs(char[][] sides) {
+        String[] pairs = new String[24];
+        String[] letters = new String[6];
+        int j = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int l = 0; l < 24; l++) {
+                letters[l] = String.valueOf(sides[i][j] + sides[i][j += 1]); //0,1
+                letters[l+=1] = String.valueOf(sides[i][j] + sides[i][j += 1]); //1,2
+            }
+        }
+        return pairs;
+    }
+
     public static void main(String[] args) {
         char[] testletters = "abcdefghijkl".toCharArray();
         char[][] testsides = defineSides(testletters);
+        printSides(testsides);
     }
 }
 
