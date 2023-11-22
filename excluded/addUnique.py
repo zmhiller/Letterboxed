f2dbf1f8642efab8c8daf654ee2dee9c7d1c439e
Zach Hiller <zmhiller@gmail.com>
2023-11-21 23:16:01 -0500

    Added unique letters array to dictionary

--- excluded/addUnique.py
+++ excluded/addUnique.py
@@ -2,27 +2,23 @@ import csv
 import os
 
 path = os.path.realpath(os.path.dirname(__file__))
-
+fields = ['Word', 'Raw Score', 'Normalized Score', 'Unique Letter Count', 'Unique Letters']
 rows = []
-with open(path + '/dictionary-test.csv', newline='') as dict:
+
+def uniqueLetters(word):
+    chars = []
+    for c in str.lower(word):
+        if c not in chars:
+            chars.append(c)
+    return sorted(chars)
+
+with open(f'{path}/dictionary-test.csv', newline='') as dict:
     dictReader = csv.reader(dict, delimiter=',', quotechar='|')
-    for row in dictReader:
-        if row[0] != 'Word':
-            unique = int(len(set(str(row[0].lower()))))
-            rows.append([row[0], int(row[1]), float(row[2]), int(row[3]), unique])
-    #print(rows[0])
-    #print(rows[1])
 
+    rows.extend([row[0], int(row[1]), float(row[2]), int(row[3]), uniqueLetters(row[0])] for row in dictReader if row[0] != 'Word')
 
-with open(path + '/NEW-dictionary-test.csv', 'w', newline='') as newDict:
-    #writer = csv.writer(newDict, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #writer.writerow(['Word', 'Raw Score', 'Normalized Score', 'Length', 'Unique Letters'])
-    fields = ['Word', 'Raw Score', 'Normalized Score', 'Length', 'Unique Letters']
+with open(f'{path}/NEW-dictionary-test.csv', 'w', newline='') as newDict:
     dictWriter = csv.DictWriter(newDict, fieldnames=fields)
     dictWriter.writeheader()
     for row in rows:
-        dictWriter.writerow({fields[0]: row[0], fields[1]: row[1], fields[2]: row[2],
-                              fields[3]: row[3], fields[4]: row[4]})
-
-        #print(row)
-        #writer.writerow(row)
+        dictWriter.writerow({fields[0]: row[0], fields[1]: row[1], fields[2]: row[2], fields[3]: row[3], fields[4]: row[4]})
