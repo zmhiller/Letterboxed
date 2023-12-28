d2ddc0ce1fe763e17fce77aae830059f2555ada4
Zach Hiller <zmhiller@gmail.com>
2023-12-28 00:12:56 -0500

    test 'A' dictionary

--- src/main/resources/dictABuild.py
+++ src/main/resources/dictABuild.py
@@ -1,27 +1,40 @@
 import csv
 
 fullDict = 'dictionaries/dictA.csv'
-letters = 'abcdefghijkl'
-illegalPairs = ["aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc", "dd", "de", "df",
+newDict = []
+testLetters = 'abcdefghijkl'
+testIllegalPairs = ["aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc", "dd", "de", "df",
             "ed", "ee", "ef", "fd", "fe", "ff", "gg", "gh", "gi", "hg", "hh", "hi", "ig", "ih", "ii", "jj", "jk",
             "jl", "kj", "kk", "kl", "lj", "lk", "ll"]
+fields = ["Word", "Normalized Score", "Unique Letter Count", "Unique Letters"]
 
 with open(fullDict,  newline="") as full:
-    
-    reader = csv.DictReader(full)
-    for row in reader:
-        add = true
-        word = row['Word']:
-        uniqueLetters = row['Unique Letters']
-        wordPairs = []
+        reader = csv.DictReader(full)
+        for row in reader:
+            word = row['Word'].lower()
+            uniqueLetters = row['Unique Letters']
+            wordPairs = []
+            for l in uniqueLetters:
+                if l not in testLetters:
+                    break
+            for char1 in word:
+                for char2 in word:
+                    wordPairs.append(char1 + char2)
+            for pair in testIllegalPairs:
+                if pair in wordPairs:
+                    break
+            newDict.append(row)
 
-        for l in uniqueLetters:
-            if l not in letters:
-                add = false
+        print(len(newDict))
+        print(newDict[100])
 
-        for char1 in word:
-            for char2 in word:
-                wordPairs.append(char1 + char2)
-        print(wordPairs)
+with open('dictionaries/testDictA.csv', 'w', newline='') as new:
+    writer = csv.DictWriter(new, fieldnames=fields, delimiter=",")
+    writer.writeheader()
+    for entry in newDict:
+        word = entry['Word']
+        score = entry['Normalized Score']
+        uniqueCount = entry['Unique Letter Count']
+        uniqueLetters = entry['Unique Letters']
 
-
+        writer.writerow({'Word': word, 'Normalized Score': score, 'Unique Letter Count': uniqueCount, 'Unique Letters': uniqueLetters})
